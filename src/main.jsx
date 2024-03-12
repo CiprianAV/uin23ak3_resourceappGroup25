28796e93c7205372a34f6a4334398a17d5bbc6a6
Ciprian Vlad <ciprianvlad000@gmail.com>
2024-03-12 15:25:56 +0100

    123123

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,29 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Route path="/html">
+        <Resources category="HTML" />
+      </Route>
+      <Route path="/css">
+        <Resources category="CSS" />
+      </Route>
+      <Route path="/javascript">
+        <Resources category="JavaScript" />
+      </Route>
+      <Route path="/react">
+        <Resources category="React" />
+      </Route>
+      <Route path="/sanity">
+        <Resources category="Sanity and headless CMS" />
+      </Route>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
